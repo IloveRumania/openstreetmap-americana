bbd1fa630d46cb275eb30087eb7b948d9d969e43
IloveRumania <48071333+IloveRumania@users.noreply.github.com>
2025-10-23 16:29:43 -0700

    Update style.ts

--- src/js/style.ts
+++ src/js/style.ts
@@ -8,6 +8,8 @@ export function build(
   glyphURL: string,
   locales: string[]
 ): StyleSpecification {
+  let today = new Date();
+  let fool = today.getMonth() === 3 && today.getDate() === 1;
   return {
     name: "Americana",
     glyphs: glyphURL,
@@ -28,6 +30,52 @@ export function build(
         tileSize: 256,
         // The actual maxzoom is 15
         maxzoom: 13,
+      },
+      controlcities: {
+        type: "geojson",
+        data: fool ? {
+          "type": "FeatureCollection",
+          "features": [
+            {
+              "type": "Feature",
+              "properties": {
+                "name": "Other Desert Cities",
+                "name:en": "Other Desert Cities",
+                "name:ja": "アザー・デザート・シティーズ",
+                "name:zh": "其他沙漠城市",
+              },
+              "geometry": {
+                "type": "Point",
+                "coordinates": [-116.3694769,33.7667042]
+              }
+            },
+            {
+              "type": "Feature",
+              "properties": {
+                "name": "Limón",
+                "name:en": "Limon",
+                "name:es": "Limón",
+                "name:uk": "Лаймон",
+                "name:sr": "Лимон",
+                "name:kk": "Лимон",
+                "name:arz": "ليمون",
+                "name:ce": "Лаймон",
+                "name:mr": "लायमन",
+                "name:ky": "Лимон",
+                "name:zh": "利蒙",
+                "name:ar": "ليمون",
+                "name:fa": "لیمون",
+                "name:tt": "Лаймон",
+                "name:bg": "Лаймън",
+                "capital": 4,
+              },
+              "geometry": {
+                "type": "Point",
+                "coordinates": [-103.6931230,39.2648545]
+              }
+            }
+          ]
+        } : {}
       },
     },
     sprite: spriteURL,
