73041728922f4c938779e87334b8a903395b7666
IloveRumania <48071333+IloveRumania@users.noreply.github.com>
2025-11-06 15:49:57 -0800

    Update shield_defs.js
    Fixed typo

--- src/js/shield_defs.js
+++ src/js/shield_defs.js
@@ -704,7 +704,7 @@ export function loadShields() {
       textColor: Color.shields.green,
       colorLighten: Color.shields.green,
     },
-    ["HIST"],
+    ["OLD"],
     Color.shields.green
   );
 
